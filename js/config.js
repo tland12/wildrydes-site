88e4a346375e1763741955eae942b9205f0c5e33
nana <tland12@naver.com>
2024-04-29 14:37:29 +0900

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_2IrccuFCP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4tb5r2g5akf1rhhu7d6ibvu34', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
